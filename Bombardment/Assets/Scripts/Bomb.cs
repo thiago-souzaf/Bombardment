d3cdc8085147616fada535e97d5944a9c350e196
Thiago de Souza Farias <tsf@cin.ufpe.br>
2024-04-01 11:43:40 -0300

    Destructible platforms

--- Bombardment/Assets/Scripts/Bomb.cs
+++ Bombardment/Assets/Scripts/Bomb.cs
@@ -1,23 +1,16 @@
-using System.Collections;
 using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
 	public float ExplosionDelay = 5.0f;
     [SerializeField] private GameObject go_explosionObject;
+    [SerializeField] private LayerMask PlatformLayer;
+    [SerializeField] private float ExplosionRadius;
+    [SerializeField] private int ExplosionDamage;
 
     private void Start()
     {
-        StartCoroutine(ExplosionCoroutine());
-    }
-
-    private IEnumerator ExplosionCoroutine()
-    {
-        // Wait
-        yield return new WaitForSeconds(ExplosionDelay);
-
-        // Explode
-        Explode();
+        Invoke(nameof(Explode), ExplosionDelay);
     }
 
     private void Explode()
@@ -26,11 +19,20 @@ public class Bomb : MonoBehaviour
         GameObject explosionFX = Instantiate(go_explosionObject, transform.position, Quaternion.identity);
         Destroy(explosionFX, 5f);
 
-        // TODO
-
         // Destroy platforms
-        // TODO
-
+        Collider[] platformColliders = Physics.OverlapSphere(transform.position, ExplosionRadius, PlatformLayer);
+        foreach(Collider collider in platformColliders)
+        {
+            if (collider.gameObject.TryGetComponent(out Life platformLife))
+            {
+                // Deal damage based on distance from explosion center
+                float distance = (collider.transform.position - transform.position).magnitude;
+                float distanceRate = Mathf.Clamp01(distance / ExplosionRadius);
+                float damageRate = 1f - Mathf.Pow(distanceRate, 4);
+                int damage = (int)Mathf.Ceil(damageRate * ExplosionDamage);
+                platformLife.TakeDamage(damage);
+            }
+        }
 
         // Destroy bomb
         Destroy(gameObject);
---
+++ Bombardment/Assets/Scripts/Life.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Life : MonoBehaviour
+{
+	public int maxHealth;
+	private int currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
